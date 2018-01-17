0e0564c24818bb91311b1d0d57a62e4938ec6112
Benny <35159859+DetectiveSax@users.noreply.github.com>
2018-01-16 18:38:44 -0800

    Hello
    Yellow?
    How did y'know?
    Well I have color ID.

---
+++ Hello world/Hello and please work/Program.cs
@@ -0,0 +1,17 @@
+﻿// A Hello World Program Made in C#
+using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the Console Window Open in Debug Mode
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
